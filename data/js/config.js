ddc11d14eafd0bd5ae7fa283b3fbedb2a07c4405
FaisalKechee <vincill4sal@gmail.com>
2023-07-07 09:21:35 +0700

    chatapp

---
+++ data/js/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCrYsxsTVqrBn-Mz17Rwq2WI5Cso-1_6PA",
+  authDomain: "database-firebase-555.firebaseapp.com",
+  databaseURL: "https://database-firebase-555-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "database-firebase-555",
+  storageBucket: "database-firebase-555.appspot.com",
+  messagingSenderId: "674929730569",
+  appId: "1:674929730569:web:29176eb215a7b25131c4d8",
+  measurementId: "G-J9FH6309T4"
+};
